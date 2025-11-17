fbddd25a97a63d1d230df3d0bcbfe714335c4ec6
Cristino Jordao <131712016+ccj96professor@users.noreply.github.com>
2025-11-17 10:52:34 -0400

    add sistema.php

---
+++ sistema.php
@@ -0,0 +1,59 @@
+<?php 
+//iniciando sessão
+session_start();
+
+// print_r($_SESSION);//imprime na tela as variaveis utilizada na sessão
+
+//verificação se a variavel sessão foi iniciada com algum valor
+if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true)){
+    unset($_SESSION['email']);//destruir a variavel que tenha a SESSION amarzenada
+    unset($_SESSION['senha']);
+    header('location: login.php');
+}else{
+    $logado= $_SESSION['email'];
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <title>Sistema</title>
+
+    <style>
+         body{
+            font-family: Arial, Helvetica, sans-serif;
+            background-image: linear-gradient(45deg, cyan, yellow);
+            color: white;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <!-- barra de nevagação bootstrap -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">SISTEMA</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+
+        <!-- botão de sair -->
+         <!-- parei no video no minutno 31:07 - https://www.youtube.com/watch?v=15CXhnQkilY&list=PLSHNk_yA5fNjoIRNHV-3FprsN3NWPcnnK&index=4 -->
+        <div class="d-flex">
+            <a href="sair.php" class="btn btn-danger me-5">Sair</a>
+        </div>
+    </nav>
+    <br>
+    <?php
+        echo "<h1>Bem vindo <u>$logado</u></h1>";
+    ?>
+
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script> -->
+</body>
+</html>
